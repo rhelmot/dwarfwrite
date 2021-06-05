b9986ecf236905f2e68dc5e574e4f245da3ad489
Audrey Dutcher <audrey@rhelmot.io>
2021-06-05 13:01:30 -0700

    Add tooling for dumping sections into an existing elf

--- dwarfwrite/elf.py
+++ dwarfwrite/elf.py
@@ -3,7 +3,10 @@ import shutil
 import subprocess
 import tempfile
 
-def dump_elf(result, arch, outfile):
+def dump_elf(result, arch, outfile, infile=None, result_update=None):
+    if result_update is None:
+        result_update = []
+
     tmp = tempfile.mkdtemp()
     with open(os.path.join(tmp, 'zero'), 'wb') as fp:
         fp.write(b'\0')
@@ -11,14 +14,28 @@ def dump_elf(result, arch, outfile):
     for f in result:
         with open(os.path.join(tmp, f), 'wb') as fp:
             fp.write(result[f])
+    for f in result_update:
+        with open(os.path.join(tmp, f), 'wb') as fp:
+            fp.write(result_update[f])
 
-    architecture = arch.linux_name
     # TODO add bfd_name to archinfo
-    abi = 'elf%s-%s' % (arch.bits, arch.linux_name.replace('_', '-'))
+    linux_name = arch.linux_name.replace('_', '-')
+    if arch.name.startswith("ARM"):
+        linux_name = ('big' if arch.memory_endness == 'Iend_BE' else 'little') + linux_name
+    abi = 'elf%s-%s' % (arch.bits, linux_name)
     extra_args = []
     for f in result:
         extra_args.append('--add-section')
         extra_args.append('.%s=%s' % (f, os.path.join(tmp, f)))
-    subprocess.check_call(['objcopy', '--input-target=binary', '--output-target=' + abi, '-S', '--remove-section=.data'] + extra_args + [os.path.join(tmp, 'zero'), outfile])
+    for f in result_update:
+        extra_args.append('--update-section')
+        extra_args.append('.%s=%s' % (f, os.path.join(tmp, f)))
+    if infile is None:
+        in_flags = ['--input-target=binary', '-S', '--remove-section=.data']
+        in_args = [os.path.join(tmp, 'zero')]
+    else:
+        in_flags = []
+        in_args = [infile]
+    subprocess.check_call(['objcopy', '--output-target=' + abi] + in_flags + extra_args + in_args + [outfile])
 
     shutil.rmtree(tmp)
